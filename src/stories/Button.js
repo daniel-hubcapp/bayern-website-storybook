9e281cf80581d13498fac9e777c6bf5e8b5206a2
Daniel Alter <daniel.united@windowslive.com>
2021-05-02 22:32:11 +0300

    🎨Primary and secondary button components

--- src/stories/Button.js
+++ src/stories/Button.js
@@ -5,17 +5,16 @@ import './button.css';
 /**
  * Primary UI component for user interaction
  */
-export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+export const Button = ({ primary, label, ...props }) => {
+  const mode = primary ? 'btn-primary' : 'btn-secondary';
+  const icon = 'https://assets.website-files.com/6052683cbd3896747b8918aa/607c2ce918280814c7167823_add_blue_24dp.svg';
   return (
-    <button
-      type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={backgroundColor && { backgroundColor }}
+    <a
+      className={['btn', mode].join(' ')}
       {...props}
     >
       {label}
-    </button>
+    </a>
   );
 };
 
@@ -24,14 +23,6 @@ Button.propTypes = {
    * Is this the principal call to action on the page?
    */
   primary: PropTypes.bool,
-  /**
-   * What background color to use
-   */
-  backgroundColor: PropTypes.string,
-  /**
-   * How large should the button be?
-   */
-  size: PropTypes.oneOf(['small', 'medium', 'large']),
   /**
    * Button contents
    */
@@ -43,8 +34,6 @@ Button.propTypes = {
 };
 
 Button.defaultProps = {
-  backgroundColor: null,
   primary: false,
-  size: 'medium',
   onClick: undefined,
 };
--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -3,11 +3,11 @@ import React from 'react';
 import { Button } from './Button';
 
 export default {
-  title: 'Example/Button',
+  title: 'Bayern Storybook/Button',
   component: Button,
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
+  // argTypes: {
+  //   backgroundColor: { control: 'color' },
+  // },
 };
 
 const Template = (args) => <Button {...args} />;
@@ -15,22 +15,15 @@ const Template = (args) => <Button {...args} />;
 export const Primary = Template.bind({});
 Primary.args = {
   primary: true,
-  label: 'Button',
+  label: 'הוספה לעגלה',
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
-  label: 'Button',
+  label: 'החל',
 };
 
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
+export const secondaryWithIcon = Template.bind({});
+Secondary.args = {
+  label: 'הוספה לעגלה',
 };
